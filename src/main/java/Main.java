1c1f0063759eb52567d3dc9771c56e2ccdab9119
rumburakcloud <96738567+rumburakcloud@users.noreply.github.com>
2022-03-07 13:00:34 +0100

    Update Main.java
    dodaj

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello Worlddddd");
         Arrays.stream(args).forEach(System.out::println);
     }
 }
